ae7d9ee0015e6c38a00da6fdd01d1013fb6c0393
Nikila Manoj <94917436+Nikilamanoj31@users.noreply.github.com>
2022-03-21 11:23:41 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,23 @@
+package pgm1;
+import java.util.Scanner;
+
+public class student {
+	
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the name:");
+		String n=sc.nextLine();
+		System.out.println("Enter the roll no:");
+		int r=sc.nextInt();
+		System.out.println("Enter the marks:");
+		int m=sc.nextInt();
+		System.out.println(n);
+		System.out.println(r);
+		System.out.println(m);
+		
+		
+		
+	}
+	
+
+}
